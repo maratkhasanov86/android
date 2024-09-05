274104b6fbe7dc0fbd628962c6f04c2787cdcb90
Smart developer <developer@yandex.ru>
2024-09-05 07:32:56 +0300

    1

--- app/src/main/java/com/example/myapplication1212/MainActivity2.java
+++ app/src/main/java/com/example/myapplication1212/MainActivity2.java
@@ -1,14 +1,32 @@
 package com.example.myapplication1212;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.List;
+
 public class MainActivity2 extends AppCompatActivity {
+    private DatabaseReference mDataBase;
+    private List<String> DiscrTasks;
+    ListView ListUserTasks;
+    private String USER_KEY = "User";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +38,32 @@ public class MainActivity2 extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        ListUserTasks = (ListView) findViewById(R.id.discr_for_tasks);
+        mDataBase = FirebaseDatabase.getInstance().getReference(USER_KEY);
+        updateUI();
+        mDataBase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                GenericTypeIndicator<List<String>> t = new GenericTypeIndicator<List<String>>(){};
+                DiscrTasks = snapshot.getValue(t);
+
+
+
+            }
+
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+
+        }
+    private void updateUI(){
+        ArrayAdapter <String> adapter = new ArrayAdapter<String>(getBaseContext(), android.R.layout.simple_list_item_1,DiscrTasks);
+        ListUserTasks.setAdapter(adapter);
     }
+
 }
